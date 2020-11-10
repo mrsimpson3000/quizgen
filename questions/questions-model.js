498b38aeaa1c8be8c07c5197fc7154a7bb5b86e6
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-11-09 23:00:04 -0500

    Updated ability to add questions to db

--- questions/questions-model.js
+++ questions/questions-model.js
@@ -3,6 +3,9 @@ const db = require("../database/dbConfig");
 module.exports = {
   find,
   findBy,
+  removeAll,
+  count,
+  addQuestions,
 };
 
 function find() {
@@ -11,6 +14,18 @@ function find() {
 
 function findBy(filter) {
   return db("questions").where(filter).where("series", 0).orderBy("id");
+}
+
+function count() {
+  return db("questions").count('id')
+}
+
+function removeAll() {
+  return db("questions").select().del()
+}
+
+function addQuestions(data) {
+  return db("questions").insert(data)
 }
 
 // function findBy(filter) {
