a812089f58715f798b87cca36701cd503e57b6ce
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-12-01 20:57:58 -0500

    Made a change to config.vars

--- config/vars.js
+++ config/vars.js
@@ -3,7 +3,7 @@ module.exports = {
   pgConnection: process.env.DATABASE_URL || "postgres://postgres:s115115744@localhost:5432/quizgen",
   rounds: parseInt(process.env.BCRYPT_ROUNDS) || 12,
   mailHost: process.env.FORM_HOST || "send.one.com",
-  mailPort: process.env.FORM_PORT || 465,
+  mailPort: parseInt(process.env.FORM_PORT) || 465,
   mailSecure: process.env.FORM_SECURE || true,
   mailUser: process.env.FORM_USER || "form@mcbiblequiz.com",
   mailPass: process.env.FORM_PASS || "EvS5WtwP5Xnyk56",
